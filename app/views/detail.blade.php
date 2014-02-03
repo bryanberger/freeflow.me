e97f06e550567c546c2af9f66dbe2a8297a48ac6
Bryan Berger <bryan@bryanberger.com>
2014-02-03 00:32:52 -0500

    test

--- app/views/detail.blade.php
+++ app/views/detail.blade.php
@@ -16,8 +16,9 @@
 			@endif
 		</ul>
 	</div>
-	
+
 	<p title="the time this post was created" class="date">This piece was created <span>{{ TimeAgo::time_passed($post->created_at) }}</span> and is #{{ $post->sequence_number }} of {{ $max_days }}</p>
+	
 	<ul class="tags">
 	@foreach ($tags as $tag)
 		<li class="tag">{{ $tag }}</li>
