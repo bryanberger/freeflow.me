328745c8c81c690c8a21b441776b8e0ea717cc23
Bryan Berger <bryan@bryanberger.com>
2014-07-22 23:55:06 -0400

    updated User class to stub out missing abstract methods

--- app/models/User.php
+++ app/models/User.php
@@ -49,4 +49,36 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 		return $this->email;
 	}
 
+
+	/**
+	 * Get the token value for the "remember me" session.
+	 *
+	 * @return string
+	 */
+	public function getRememberToken()
+	{
+		
+	}
+
+	/**
+	 * Set the token value for the "remember me" session.
+	 *
+	 * @param  string  $value
+	 * @return void
+	 */
+	public function setRememberToken($value)
+	{
+
+	}
+
+	/**
+	 * Get the column name for the "remember me" token.
+	 *
+	 * @return string
+	 */
+	public function getRememberTokenName()
+	{
+
+	}
+
 }
