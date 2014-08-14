fd1aeb73adc218b82f60ba1951f3a31e31ecbc24
Bryan Berger <bryan@bryanberger.com>
2014-08-14 14:15:06 -0400

    added css3 hover transition for each freeflow on home page

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -6,20 +6,20 @@
 
 @if($posts->getCurrentPage() === 1)
 	@if ($i === 0)
-		<li class="column grid-first"><a href="/{{ $p->filename }}"><div class="num">{{ $p->id . " of 365"}}</div><img src="{{ $cdn_path . $p->filename}}_560.jpg"></a></li>
+		<li class="column grid-first"><a href="/{{ $p->filename }}"><div class="title"><div><span class="num">#{{ $p->id }}</span>{{" : $p->name"}}</div></div><img src="{{ $cdn_path . $p->filename}}_560.jpg"></a></li>
 	@elseif ($i === 1)
-		<li class="column grid1 alt1 alt2"><a href="/{{ $p->filename }}"><img src="{{ $cdn_path . $p->filename}}_560.jpg"></a></li>
+		<li class="column grid1 alt1 alt2"><a href="/{{ $p->filename }}"><div class="title"><div><span class="num">#{{ $p->id }}</span>{{" : $p->name"}}</div></div><img src="{{ $cdn_path . $p->filename}}_560.jpg"></a></li>
 	@elseif ($i === 2)
-		<li class="column grid1"><a href="/{{ $p->filename }}"><img src="{{ $cdn_path . $p->filename}}_560.jpg"></a></li>
+		<li class="column grid1"><a href="/{{ $p->filename }}"><div class="title"><div><span class="num">#{{ $p->id }}</span>{{" : $p->name"}}</div></div><img src="{{ $cdn_path . $p->filename}}_560.jpg"></a></li>
 	@elseif ($i === 3)
-		<li class="column grid1 alt2"><a href="/{{ $p->filename }}"><img src="{{ $cdn_path . $p->filename}}_560.jpg"></a></li>
+		<li class="column grid1 alt2"><a href="/{{ $p->filename }}"><div class="title"><div><span class="num">#{{ $p->id }}</span>{{" : $p->name"}}</div></div><img src="{{ $cdn_path . $p->filename}}_560.jpg"></a></li>
 	@elseif ($i === 4)
-		<li class="column grid1 alt1"><a href="/{{ $p->filename }}"><img src="{{ $cdn_path . $p->filename}}_560.jpg"></a></li>
+		<li class="column grid1 alt1"><a href="/{{ $p->filename }}"><div class="title"><div><span class="num">#{{ $p->id }}</span>{{" : $p->name"}}</div></div><img src="{{ $cdn_path . $p->filename}}_560.jpg"></a></li>
 	@else
-		<li class="column grid1"><a href="/{{ $p->filename }}"><img src="{{ $cdn_path . $p->filename}}_560.jpg"></a></li>
+		<li class="column grid1"><a href="/{{ $p->filename }}"><div class="title"><div><span class="num">#{{ $p->id }}</span>{{" : $p->name"}}</div></div><img src="{{ $cdn_path . $p->filename}}_560.jpg"></a></li>
 	@endif
 @else
-	<li class="column grid1"><a href="/{{ $p->filename }}"><img src="{{ $cdn_path . $p->filename}}_560.jpg"></a></li>
+	<li class="column grid1"><a href="/{{ $p->filename }}"><div class="title"><div><span class="num">#{{ $p->id }}</span>{{" : $p->name"}}</div></div><img src="{{ $cdn_path . $p->filename}}_560.jpg"></a></li>
 @endif
 
 @endfor
