af5cf692c5f590ea2ffe0077884d31cbff4f804b
Bryan Berger <bryan@bryanberger.com>
2014-12-02 12:40:00 -0500

    remove cybermonday coupon code

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -38,8 +38,7 @@
 		<!--[if lt IE 8]>
 			<p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
 		<![endif]-->
-
-		<div id="global">[CYBER MONDAY] Use coupon code <b>CYBER25</b> when checking out for a <b>25%</b> discount all day today!</div>
+		<div id="global">Use coupon code <b>LOVEYOUGUYS</b> when checking out for a <b>10%</b> discount off your entire cart!</div>
 		<main role="main" class="container">
 			
 			<header>
