769dfedbbaea89076369940a0e35fedc6fceac74
Bryan Berger <bryan@bryanberger.com>
2015-04-06 13:14:51 -0400

    use jquery's cdn instead

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -26,8 +26,8 @@
 			r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
 			ga('create','UA-640481-18');ga('send','pageview');
 		</script>
-
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+		
+		<script src="//code.jquery.com/ui/1.10.4/jquery-ui.min.js"></script>
 		{{ HTML::style('assets/css/styles.min.css'); }}
 		<!--[if lt IE 9]>
 			<script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
