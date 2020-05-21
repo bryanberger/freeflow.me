3bd13067c5f8b71485406be375cd0770566c3f3d
Bryan Berger <bryan@bryanberger.com>
2020-05-21 11:37:41 -0400

    prettier, and remove references to http://

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -1,6 +1,7 @@
 <?php
 
-class BaseController extends Controller {
+class BaseController extends Controller
+{
 
 	/**
 	 * Setup the layout used by the controller.
@@ -9,16 +10,16 @@ class BaseController extends Controller {
 	 */
 	protected function setupLayout()
 	{
-		if ( ! is_null($this->layout))
-		{
+		if (!is_null($this->layout)) {
 			$this->layout = View::make($this->layout);
 		}
 	}
 
-	protected function getCDNPath($name) {
-		return 'http://freeflow.me/assets/imgs/art/';
-		
-		if($name === NULL) {
+	protected function getCDNPath($name)
+	{
+		return 'https://freeflow.me/assets/imgs/art/';
+
+		if ($name === NULL) {
 			$name = 'humhum_560'; // humhum is the smallest image filesize wise.
 		}
 
@@ -27,19 +28,18 @@ class BaseController extends Controller {
 			$status_code = Cache::get('dropbox_status_code');
 		} else {
 			// we don't have a cached status_code so grab one and store it for a few hours
-			$request = Requests::get('http://dl.dropboxusercontent.com/u/584602/freeflow.me/imgs/art/'.$name.'.jpg');
+			$request = Requests::get('https://dl.dropboxusercontent.com/u/584602/freeflow.me/imgs/art/' . $name . '.jpg');
 			$status_code = $request->status_code;
 			Cache::put('dropbox_status_code', $status_code, 60);
 		}
 
 		// if dropbox has blocked us because of bandwidth limits, use local files
-		if($status_code === 509 || $status_code  === 404) {
-			$cdn_path = 'http://freeflow.me/assets/imgs/art/';
+		if ($status_code === 509 || $status_code  === 404) {
+			$cdn_path = 'https://freeflow.me/assets/imgs/art/';
 		} else {
-			$cdn_path = 'http://dl.dropboxusercontent.com/u/584602/freeflow.me/imgs/art/';
+			$cdn_path = 'https://dl.dropboxusercontent.com/u/584602/freeflow.me/imgs/art/';
 		}
 
 		return $cdn_path;
 	}
-
 }
--- public/js/cart.js
+++ public/js/cart.js
@@ -1,324 +1,318 @@
-$(function(){
-	var shippingRate = 26.00; 
-	var fadeTime = 125;
-	var total;
-	var coupon = {
-		value: 0,
-		code: ""
-	};
-	//var discount = 0;
+$(function () {
+  var shippingRate = 26.0
+  var fadeTime = 125
+  var total
+  var coupon = {
+    value: 0,
+    code: '',
+  }
+  //var discount = 0;
 
-	function updateQuantityIcon(val) {
-		if(val == 0) {
-			$('.cart-quantity').hide();
-			return;
-		}
+  function updateQuantityIcon(val) {
+    if (val == 0) {
+      $('.cart-quantity').hide()
+      return
+    }
 
-		$('.cart-quantity').fadeOut(250, function(){
-			$(this).fadeIn(250);
-			$('.cart-quantity').text(val);
-		});
-	}
+    $('.cart-quantity').fadeOut(250, function () {
+      $(this).fadeIn(250)
+      $('.cart-quantity').text(val)
+    })
+  }
 
-	function shake($highlightElem, $element) {
-		// highlight error
-		$highlightElem.addClass('highlight');
+  function shake($highlightElem, $element) {
+    // highlight error
+    $highlightElem.addClass('highlight')
 
-		// error
-		$element.addClass('animated shake');
-		setTimeout(function(){
-			$element.removeClass('animated shake');
-		}, 1000);
-	}
+    // error
+    $element.addClass('animated shake')
+    setTimeout(function () {
+      $element.removeClass('animated shake')
+    }, 1000)
+  }
 
-	// coupons
-	$('#applycoupon').on('click', function(){
-		$coupon = $('#coupon');
+  // coupons
+  $('#applycoupon').on('click', function () {
+    $coupon = $('#coupon')
 
-		$.ajax({
-			url: '/buy/validate',
-			type: 'post',
-			data: {coupon: $coupon.val()},
-			success: function(data) {
-				json = JSON.parse(data);
+    $.ajax({
+      url: '/buy/validate',
+      type: 'post',
+      data: { coupon: $coupon.val() },
+      success: function (data) {
+        json = JSON.parse(data)
 
-				if (typeof json.success !== 'undefined' && $.isNumeric(json.success)) {
-					coupon.value = json.success;
-					coupon.id = $coupon.val();
-					$coupon.removeClass('highlight');
+        if (typeof json.success !== 'undefined' && $.isNumeric(json.success)) {
+          coupon.value = json.success
+          coupon.id = $coupon.val()
+          $coupon.removeClass('highlight')
 
-					recalculateCart();
-				} else {
-					coupon.value = 0;
-					shake($coupon, $coupon);
-					recalculateCart();
-				}
-			},
-			error: function(data) {
-				coupon.value = 0;
-				shake($coupon, $coupon);
-				recalculateCart();
-			}
-		}); // end ajax call
-	});
+          recalculateCart()
+        } else {
+          coupon.value = 0
+          shake($coupon, $coupon)
+          recalculateCart()
+        }
+      },
+      error: function (data) {
+        coupon.value = 0
+        shake($coupon, $coupon)
+        recalculateCart()
+      },
+    }) // end ajax call
+  })
 
-	// Trigger animation on Add to Cart button click
-	function addtocartclick() {
-		$(this).addClass('active');
-		$('.addtocart').off( "click");
+  // Trigger animation on Add to Cart button click
+  function addtocartclick() {
+    $(this).addClass('active')
+    $('.addtocart').off('click')
 
-		$.ajax({
-			url: 'buy/' + $(this).data('id'),
-			type: 'post',
-			data: {size: $(this).data('size')},
-			success: function(data) {
-				json = JSON.parse(data);
+    $.ajax({
+      url: 'buy/' + $(this).data('id'),
+      type: 'post',
+      data: { size: $(this).data('size') },
+      success: function (data) {
+        json = JSON.parse(data)
 
-				if (typeof json.success !== 'undefined' && $.isNumeric(json.success)) {
-					//console.log("Item Added!");
-					updateQuantityIcon(json.success);
-				} else {
-					//console.log("Item Failed");
-				}
-			},
-			error: function(data) {
-				//console.log("Ajax Error!");
-			}
-		}); // end ajax call
+        if (typeof json.success !== 'undefined' && $.isNumeric(json.success)) {
+          //console.log("Item Added!");
+          updateQuantityIcon(json.success)
+        } else {
+          //console.log("Item Failed");
+        }
+      },
+      error: function (data) {
+        //console.log("Ajax Error!");
+      },
+    }) // end ajax call
 
+    setTimeout(function () {
+      $('.addtocart').removeClass('active')
+      $('.addtocart').on('click', addtocartclick)
+    }, 1000)
+  }
+  $('.addtocart').on('click', addtocartclick)
 
-		setTimeout(function () {
-			$('.addtocart').removeClass('active');
-			$('.addtocart').on('click',addtocartclick);
-		}, 1000);
-	}
-	$('.addtocart').on('click', addtocartclick);
+  // Checkout click handler
+  $('.checkout').on('click', function () {
+    if ($('.country').val() == '--') {
+      shake($('.country'), $('.checkout'))
+      return
+    }
 
-	// Checkout click handler
-	$('.checkout').on('click', function(){
+    $('.country').removeClass('highlight')
 
-		if($('.country').val() == "--") {
-			shake($('.country'), $('.checkout'));
-			return;
-		}
+    var desc
+    var handler = StripeCheckout.configure({
+      key: stripeToken,
+      shippingAddress: true,
+      billingAddress: false,
+      allowRememberMe: true,
+      zipCode: true,
+      image: '//freeflow.me/assets/imgs/secure.png',
+      token: function (token, args) {
+        $.ajax({
+          url: '/buy/checkout',
+          type: 'post',
+          data: {
+            token: token.id,
+            email: token.email,
+            card: token.card,
+            coupon_id: coupon.id,
+            coupon_value: coupon.value,
+          },
+          success: function (data) {
+            json = JSON.parse(data)
 
-		$('.country').removeClass('highlight');
+            if (typeof json.success !== 'undefined') {
+              ga('send', 'event', 'buy', 'checkout', 'complete')
+              $('.shopping-cart').html(
+                '<h3><span>Success!</span> Check your email for your order confirmation.</h3><p>We use a 3rd party printing company to produce the freeflows. Typically orders will take 5-10 business days for a complete turnaround.</p><p>The smaller 12x12" prints ship flat. The larger 24x24" prints ship in a heavy duty tube. We will send you an email with your tracking number once it has shipped!</p>',
+              )
+              removeAllItems()
+            } else {
+              ga('send', 'event', 'buy', 'checkout', 'error')
+              //console.log("Success Error...");
+            }
+          },
+          error: function (data) {
+            ga('send', 'event', 'buy', 'checkout', 'error')
+            //console.log("Ajax Error...");
+          },
+        })
+      },
+    })
 
-		var desc;
-		var handler = StripeCheckout.configure({
-			key: stripeToken,
-			shippingAddress: true,
-			billingAddress: false,
-			allowRememberMe: true,
-			zipCode: true,
-			image: "http://freeflow.me/assets/imgs/secure.png",
-			token: function(token, args) {
-				$.ajax({
-					url: '/buy/checkout',
-					type: 'post',
-					data: {token:token.id, email:token.email, card:token.card, coupon_id:coupon.id, coupon_value:coupon.value},
-					success: function(data) {
-						json = JSON.parse(data);
+    // call buy service
+    handler.open({
+      name: 'Freeflow.me',
+      description: $('.cart-quantity').text() + ' print(s) ($' + total + ')',
+      amount: total * 100,
+    })
 
-						if (typeof json.success !== 'undefined') {
-							ga('send', 'event', 'buy', 'checkout', 'complete');
-							$('.shopping-cart').html('<h3><span>Success!</span> Check your email for your order confirmation.</h3><p>We use a 3rd party printing company to produce the freeflows. Typically orders will take 5-10 business days for a complete turnaround.</p><p>The smaller 12x12" prints ship flat. The larger 24x24" prints ship in a heavy duty tube. We will send you an email with your tracking number once it has shipped!</p>');
-							removeAllItems();
-						} else {
-							ga('send', 'event', 'buy', 'checkout', 'error');
-							//console.log("Success Error...");
-						}
+    return false
+  })
 
-					},
-					error: function(data) {
-						ga('send', 'event', 'buy', 'checkout', 'error');
-						//console.log("Ajax Error...");
-					}
-				});			  
-			}
-		});
+  /* Assign actions */
+  $('#coupon').keyup(function () {
+    $(this).removeClass('highlight')
+  })
 
-		// call buy service
-		handler.open({
-			name: 'Freeflow.me',
-			description: $('.cart-quantity').text() + ' print(s) ($' + total + ')',
-			amount: total * 100
-		});
+  $('.product-quantity input').change(function () {
+    updateQuantity(this)
+  })
 
-		return false;
-	});
+  $('.product-quantity input').keyup(function () {
+    updateQuantity(this)
+  })
 
-	/* Assign actions */
-	$('#coupon').keyup( function() {
-	  $(this).removeClass('highlight');
-	});
+  $('.product-removal button').click(function () {
+    removeItem(this)
+  })
 
-	$('.product-quantity input').change( function() {
-	  updateQuantity(this);
-	});
+  $('select.country').change(function () {
+    if ($(this).val() != '--') {
+      $('.country').removeClass('highlight')
+    } else {
+      $('.country').addClass('highlight')
+    }
 
-	$('.product-quantity input').keyup( function() {
-	  updateQuantity(this);
-	});
+    recalculateCart()
+  })
 
-	$('.product-removal button').click( function() {
-	  removeItem(this);
-	});
+  /* Recalculate cart */
+  function recalculateCart() {
+    var subtotal = 0
 
-	$('select.country').change( function() {
-		if($(this).val() != "--") {
-			$('.country').removeClass('highlight');
-		} else {
-			$('.country').addClass('highlight');
-		}
+    /* Sum up row totals */
+    $('.product').each(function () {
+      subtotal += parseFloat($(this).children('.product-line-price').text())
+    })
 
-	  	recalculateCart();
-	});
+    // calc shipping
+    country = $('.country').val()
+    if (country === 'US') {
+      shippingRate = 7.0
+    } else if (country === 'CA') {
+      shippingRate = 15.0
+    } else if (country === '--') {
+      shippingRate = 0.0
+    } else {
+      shippingRate = 26.0
+    }
 
+    /* Calculate totals */
+    // var tax = subtotal * taxRate;
+    var shipping = subtotal > 0 ? shippingRate : 0
+    //var total = subtotal + tax + shipping;
+    var discountValue = subtotal * (coupon.value / 100)
+    total = subtotal - discountValue + shipping
 
-	/* Recalculate cart */
-	function recalculateCart()
-	{
-	  var subtotal = 0;
-	  
-	  /* Sum up row totals */
-	  $('.product').each(function () {
-	    subtotal += parseFloat($(this).children('.product-line-price').text());
-	  });
+    /* Update totals display */
+    $('.totals-value').fadeOut(fadeTime, function () {
+      $('#cart-subtotal').html(subtotal.toFixed(2))
+      $('#cart-discount').html('-' + discountValue.toFixed(2))
+      $('#cart-shipping').html(shipping.toFixed(2))
+      $('#cart-total').html(total.toFixed(2))
+      if (total == 0) {
+        $('.checkout').fadeOut(fadeTime)
+      } else {
+        $('.checkout').fadeIn(fadeTime)
+      }
+      $('.totals-value').fadeIn(fadeTime)
+    })
+  }
 
-	  // calc shipping
-	  country = $('.country').val();
-	  if(country === "US") {
-	  	shippingRate = 7.00;
-	  } else if(country === "CA") {
-	  	shippingRate = 15.00;
-	  } else if(country === "--") {
-	  	shippingRate = 0.00;
-	  } else {
-	  	shippingRate = 26.00;
-	  }
-	  
-	  /* Calculate totals */
-	 // var tax = subtotal * taxRate;
-	  var shipping = (subtotal > 0 ? shippingRate : 0);
-	  //var total = subtotal + tax + shipping;
-	  var discountValue = subtotal * (coupon.value/100);
-	  total = (subtotal - discountValue) + shipping;
-	  
-	  /* Update totals display */
-	  $('.totals-value').fadeOut(fadeTime, function() {
-	    $('#cart-subtotal').html(subtotal.toFixed(2));
-	    $('#cart-discount').html("-" + discountValue.toFixed(2));
-	    $('#cart-shipping').html(shipping.toFixed(2));
-	    $('#cart-total').html(total.toFixed(2));
-	    if(total == 0){
-	      $('.checkout').fadeOut(fadeTime);
-	    }else{
-	      $('.checkout').fadeIn(fadeTime);
-	    }
-	    $('.totals-value').fadeIn(fadeTime);
-	  });
-	}
+  /* Update quantity */
+  function updateQuantity(quantityInput) {
+    /* Calculate line price */
+    var productRow = $(quantityInput).parent().parent()
+    var price = parseFloat(productRow.children('.product-price').text())
+    var quantity = $(quantityInput).val()
+    var linePrice = price * quantity
 
+    /* Update line price display and recalc cart totals */
+    productRow.children('.product-line-price').each(function () {
+      $(this).fadeOut(fadeTime, function () {
+        $(this).text(linePrice.toFixed(2))
+        recalculateCart()
+        $(this).fadeIn(fadeTime)
+      })
+    })
 
-	/* Update quantity */
-	function updateQuantity(quantityInput)
-	{
-	  /* Calculate line price */
-	  var productRow = $(quantityInput).parent().parent();
-	  var price = parseFloat(productRow.children('.product-price').text());
-	  var quantity = $(quantityInput).val();
-	  var linePrice = price * quantity;
-	  
-	  /* Update line price display and recalc cart totals */
-	  productRow.children('.product-line-price').each(function () {
-	    $(this).fadeOut(fadeTime, function() {
-	      $(this).text(linePrice.toFixed(2));
-	      recalculateCart();
-	      $(this).fadeIn(fadeTime);
-	    });
-	  });  
+    // update service
+    $.ajax({
+      url: '/buy/update/' + productRow.data('id'),
+      type: 'post',
+      data: { qty: quantity },
+      success: function (data) {
+        json = JSON.parse(data)
 
-		// update service
-		$.ajax({
-			url: '/buy/update/' + productRow.data('id'),
-			type: 'post',
-			data: {qty: quantity},
-			success: function(data) {
-				json = JSON.parse(data);
+        if (typeof json.success !== 'undefined' && $.isNumeric(json.success)) {
+          //console.log("Item Quantity updated!");
+          updateQuantityIcon(json.success)
+        } else {
+          //console.log("Item Quantity Error...");
+        }
+      },
+      error: function (data) {
+        //console.log("Ajax Error!");
+      },
+    })
+  }
 
-				if (typeof json.success !== 'undefined' && $.isNumeric(json.success)) {
-					//console.log("Item Quantity updated!");
-					updateQuantityIcon(json.success);
-				} else {
-					//console.log("Item Quantity Error...");
-				}
+  /* Remove item from cart */
+  function removeItem(removeButton) {
+    /* Remove row from DOM and recalc cart total */
+    var productRow = $(removeButton).parent().parent()
+    productRow.slideUp(fadeTime, function () {
+      productRow.remove()
+      recalculateCart()
+    })
 
-			},
-			error: function(data) {
-				//console.log("Ajax Error!");
-			}
-		});
-	}
+    // remove single item
+    $.ajax({
+      url: '/buy/remove/' + productRow.data('id'),
+      type: 'post',
+      success: function (data) {
+        json = JSON.parse(data)
 
+        if (typeof json.success !== 'undefined' && $.isNumeric(json.success)) {
+          //console.log("Item Removed!");
+          updateQuantityIcon(json.success)
+        } else {
+          //console.log("Item Removed Error...");
+        }
+      },
+      error: function (data) {
+        //console.log("Ajax Error!");
+      },
+    })
+  }
 
-	/* Remove item from cart */
-	function removeItem(removeButton)
-	{
-	  /* Remove row from DOM and recalc cart total */
-	  var productRow = $(removeButton).parent().parent();
-	  productRow.slideUp(fadeTime, function() {
-	    productRow.remove();
-	    recalculateCart();
-	  });
+  function removeAllItems() {
+    // destroy cart
+    $.ajax({
+      url: '/buy/destroy/',
+      type: 'post',
+      success: function (data) {
+        json = JSON.parse(data)
 
-		// remove single item
-		$.ajax({
-			url: '/buy/remove/' + productRow.data('id'),
-			type: 'post',
-			success: function(data) {
-				json = JSON.parse(data);
+        if (typeof json.success !== 'undefined' && $.isNumeric(json.success)) {
+          //console.log("Items Removed!");
+          $('.product').each(function () {
+            $(this).remove()
+          })
+          updateQuantityIcon(json.success)
+        } else {
+          //console.log("Items Removed Error...");
+        }
+      },
+      error: function (data) {
+        //console.log("Ajax Error!");
+      },
+    })
+  }
 
-				if (typeof json.success !== 'undefined' && $.isNumeric(json.success)) {
-					//console.log("Item Removed!");
-					updateQuantityIcon(json.success);
-				} else {
-					//console.log("Item Removed Error...");
-				}
-
-			},
-			error: function(data) {
-				//console.log("Ajax Error!");
-			}
-		});
-	}
-
-
-	function removeAllItems()
-	{
-		// destroy cart
-		$.ajax({
-			url: '/buy/destroy/',
-			type: 'post',
-			success: function(data) {
-				json = JSON.parse(data);
-
-				if (typeof json.success !== 'undefined' && $.isNumeric(json.success)) {
-					//console.log("Items Removed!");
-					$('.product').each(function () {
-						$(this).remove();
-					});
-					updateQuantityIcon(json.success);
-				} else {
-					//console.log("Items Removed Error...");
-				}
-
-			},
-			error: function(data) {
-				//console.log("Ajax Error!");
-			}
-		});
-	}
-
-	recalculateCart();
-});
+  recalculateCart()
+})
